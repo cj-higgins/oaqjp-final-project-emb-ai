252712166b128779f57d262dc84330ce6f3150cc
CJ Higgins <ch.ja.higgins@gmail.com>
2025-05-02 12:22:13 -0400

    Add emotion_detector function

---
+++ emotion_detection.py
@@ -0,0 +1,8 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    URL= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    data= { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL, json=data, headers=Headers)
+    return response.text
